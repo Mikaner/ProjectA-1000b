67d73b4985c2431187c10fb2409906ab812b23cd
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-05-20 18:52:23 +0900

    Change langage from java to unity

---
+++ Wakaholiric/Assets/Scripts/TextController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public string[] scenarios;
+    public Text uiText;
+
+    int currentLine = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextUpdate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentLine < scenarios.Length && Input.GetMouseButtonDown(0)) {
+            TextUpdate();
+        }
+    }
+
+    void TextUpdate() {
+        uiText.text = scenarios[currentLine];
+        currentLine ++;
+    }
+}
--- game/shooting/stageobj/MainCharacter.java
+++ game/shooting/stageobj/MainCharacter.java
@@ -1,8 +1,27 @@
 package shooting.stageobj;
 
-public class MainCharacter extends Human {
-    public MainCharacter(int hp, int radius,int locationX, int locationY){
-        super(hp, radius, locationX, locationY);
+public class Player extends GameObject {
+    double speed;
+
+    public Player(double x, double y, double speed){
+        super.x = x;
+        super.y = y;
+        this.speed = speed;
+        super.active = false;
+    }
+
+    public void main(){}
+
+    public void main(int ix, int iy) {
+        double postX = x + ix * speed;
+        double postY = y + iy * speed;
+
+        if ((0 < postX)&&(postX < 500)) {
+            x = postX;
+        }
+        if ((0 < postY)&&(postY < 500)) {
+            y = posty;
+        }
     }
 
     @Override
